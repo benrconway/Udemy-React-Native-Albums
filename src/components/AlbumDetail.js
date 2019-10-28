63a8bb5d1ff289e8c1a53b8cf28578f690911e69
Ben Conway <benrconway84@gmail.com>
2019-10-28 16:30:38 +0000

    Adding content to card sections

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { Text, Button, StyleSheet, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = ({ album:{ title, artist, image, url }}) => {
   return (
-    <View style={styles.container}>
-      <Text>{title}</Text>
-      <Text>{artist}</Text>
-      <Image style={styles.image} source={{uri: image}}/>
-      <Text>{url}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <Text>{title}</Text>
+      </CardSection>
+      <CardSection>
+        <Image style={styles.image} source={{uri: image}}/>
+      </CardSection>
+      <CardSection>
+        <Button title="Buy Now"/>
+      </CardSection>
+
+    </Card>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginVertical: 20,
-    justifyContent: 'space-evenly'
-  },
   image: {
     height: 50,
     width: 50
